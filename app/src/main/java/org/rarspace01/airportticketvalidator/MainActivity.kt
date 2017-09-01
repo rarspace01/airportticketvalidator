f432295d692ba41b062e6b8976e00e8bc944513a
rarspace01 <rarspace01@gmail.com>
2017-09-01 16:39:40 +0200

    ADD: volley request

--- app/src/main/java/org/rarspace01/airportticketvalidator/MainActivity.kt
+++ app/src/main/java/org/rarspace01/airportticketvalidator/MainActivity.kt
@@ -4,7 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.EditText
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.zxing.integration.android.IntentIntegrator
 
 
@@ -14,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         IntentIntegrator(this).setBeepEnabled(false).setOrientationLocked(false).initiateScan()
+        var airportText = findViewById <EditText>(R.id.txtAirport)
+        getDepartingFlights(airportText.text.toString())
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
@@ -30,6 +37,20 @@ class MainActivity : AppCompatActivity() {
             // This is important, otherwise the result will not be passed to the fragment
             super.onActivityResult(requestCode, resultCode, data)
         }
+    }
+
+    fun getDepartingFlights(airportCode: String) {
+        //
+        var departedPage = "https://api.lufthansa.com/v1/operations/flightstatus/departures/" + airportCode + "/2017-09-01T16:00?limit=100"
+        val queue = Volley.newRequestQueue(this)
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(Request.Method.GET, departedPage,
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+                    Log.d(this.localClassName, response)
+                }, Response.ErrorListener { Log.e(this.localClassName, "error") })
+        queue.add(stringRequest)
+
     }
 
 }
