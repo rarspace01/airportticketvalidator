b0a1a1777ae47dd730df57a5f53c9dba9bf41092
rarspace01 <rarspace01@gmail.com>
2017-09-19 23:35:20 +0200

    just in Case

--- app/src/main/java/org/rarspace01/airportticketvalidator/Flight.java
+++ app/src/main/java/org/rarspace01/airportticketvalidator/Flight.java
@@ -7,9 +7,9 @@ public class Flight {
 	public String toAirport;
 	public String flightName;
 	public String flightCarrierMarketed;
-	public int flightNumberMarketed;
+	public int flightNumberMarketed = -1;
 	public String flightCarrierOperated;
-	public int flightNumberOperated;
+	public int flightNumberOperated = -1;
 	public Date flightTime;
 	public String aircraft;
 }
